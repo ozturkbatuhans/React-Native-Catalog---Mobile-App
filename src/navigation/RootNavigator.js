73d619e80dcc0cfb762d1ac8813b416df3afd65a
ozturkbatuhans <ahmetozturk1358@gmail.com>
2025-11-07 18:40:02 +0100

    fix(navigation): disable Tab header to avoid double headers; keep Stack headers for detail

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,8 +1,8 @@
+// src/navigation/RootNavigator.js
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import HomeScreen from "../screens/HomeScreen";
 import DetailScreen from "../screens/DetailScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -12,9 +12,13 @@ const Stack = createNativeStackNavigator();
 
 function HomeStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: true }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Detail" component={DetailScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+      }}
+    >
+      <Stack.Screen name="HomeList" component={HomeScreen} options={{ title: "Home" }} />
+      <Stack.Screen name="Detail" component={DetailScreen} options={{ title: "Detail" }} />
     </Stack.Navigator>
   );
 }
@@ -22,12 +26,12 @@ function HomeStack() {
 export default function RootNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen
-          name="HomeTab"
-          component={HomeStack}
-          options={{ title: "Home", tabBarShowLabel: true }}
-        />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeStack} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
